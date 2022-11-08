8fac4f39106c7aaa9ae8ddf5d16885563cffa026
ioncrisa2 <zyohanes99@gmail.com>
2022-11-08 10:18:39 +0700

    create new forum service

---
+++ app/Services/ForumService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\Forum;
+use App\Traits\AuthUser;
+use Illuminate\Support\Str;
+
+class ForumService
+{
+    use AuthUser;
+
+    public function showAll()
+    {
+        return Forum::all();
+    }
+
+    public function storeData($data)
+    {
+        $user = $this->getAuthUser();
+
+        $user->forums()->create([
+            'title'         => $data['title'],
+            'body'          => $data['body'],
+            'slug'          => Str::slug($data['title'], '-'),
+            'category'      => $data['category']
+        ]);
+    }
+
+    public function showDetail($id)
+    {
+        return Forum::findOrFail($id);
+    }
+
+    public function updateData($id, $data)
+    {
+        $forum = Forum::findOrFail($id);
+        $this->checkOwnership($forum->id);
+        $forum->update([
+            'title'     => $data['title'],
+            'body'      => $data['body'],
+            'category'  => $data['category']
+        ]);
+
+        return $forum;
+    }
+
+    public function deleteData($id)
+    {
+        $forum = Forum::findOrFail($id)->first;
+        $this->getAuthUser();
+        $this->checkOwnership($forum->user_id);
+
+        $forum->delete();
+    }
+}
