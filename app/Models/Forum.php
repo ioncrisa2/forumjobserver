3435fa0477a0a2400766586886529fb73afc5f59
ioncrisa2 <zyohanes99@gmail.com>
2022-12-27 23:06:47 +0700

    add accessor for title

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -2,26 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use App\Traits\IndonesiaTimeFormat;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Forum extends Model
 {
-    use HasFactory,IndonesiaTimeFormat;
+    use HasFactory, IndonesiaTimeFormat;
 
-    protected $fillable = ['title','body','slug','category','user_id'];
+    protected $fillable = ['title', 'body', 'slug', 'category', 'user_id'];
 
-    protected $with = ['user','comments'];
+    protected $with = ['user', 'comments'];
+
+    public function getTitleAttribute($value)
+    {
+        return Str::title($value);
+    }
 
     public function user()
     {
-        return $this->belongsTo(User::class)->select(['id','username']);
+        return $this->belongsTo(User::class)->select(['id', 'username']);
     }
 
     public function comments()
     {
-        return $this->hasMany(Comment::class)->select(['id','forum_id','body','user_id','created_at']);
+        return $this->hasMany(Comment::class)->select(['id', 'forum_id', 'body', 'user_id', 'created_at']);
     }
-
 }
