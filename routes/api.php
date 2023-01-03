50e7017b87a0102e61e9527501268518189553f4
ioncrisa2 <zyohanes99@gmail.com>
2023-01-03 18:59:04 +0700

    remove unused import

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', RegisterController::class);
